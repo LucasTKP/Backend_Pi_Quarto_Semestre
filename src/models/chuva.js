41a6d9afb3205e435d140c97ad9a2692783221c9
Lucas Gean dos Santos <lucasgeansantos@gmail.com>
2024-06-10 21:26:21 -0300

    Update chuva.js

--- src/models/chuva.js
+++ src/models/chuva.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Chuva = mongoose.model('analise',{
+const Chuva = mongoose.model('analise_chuva',{
     temperatura: Number,
     umidade: Number,
     data: Date,
